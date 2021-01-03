5eff61c13d8ede60db51ed8b72b4c8f9171d98ef
Mando Estrada <libryum33@hotmail.com>
2021-01-02 19:48:40 -0800

    deleting unwanted files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path="/about">
           <About></About>
         </Route>
-        <Route path="/" exact>
+        <Route exact path="/" exact>
           <Starter></Starter>
         </Route>
       </Switch>
